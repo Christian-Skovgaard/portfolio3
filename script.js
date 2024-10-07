65b11c121f9243aa27917fabb39944e7abd4b53f
Christian-Skovgaard <cellochr@gmail.com>
2024-10-07 13:56:04 +0200

    added getRandom

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+//make correct number ✅
+//make input a let in js
+//compare imputted number and correct number
+//make if-statement that increase if not correct
+//make highscore system work
+//make game reset
+
+
+function getRandomNumber () {
+    return Math.floor(Math.random() * 21)
+}
+const correctNumber =getRandomNumber()
+
+console.log(correctNumber)
+
+const playerInputField =  document.querySelector('input')
+const guessButton = document.querySelector('.')
+
+//button class is "guess"
