eb2c0dee3abbe4dd7a581d9a8148871f134ed450
Christian-Skovgaard <cellochr@gmail.com>
2024-10-07 16:35:00 +0200

    all works!!!!

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 //make correct number ✅
 //compare imputted number and correct number ✅
 //make if-statement that increase if not correct
-//make highscore system work
+//make highscore system work ✅
 //make game reset
 
 //assign correct answer
@@ -11,17 +11,19 @@ function getRandomNumber () {
     return Math.floor(Math.random() * 21)
 }
 let correctNumber =getRandomNumber()
-
+console.log(correctNumber)
 
 //assign html elements
 const playerInputField =  document.querySelector('input')
 const checkButton = document.querySelector(".check")
 const scoreField = document.querySelector('.score')
 const highscoreField = document.querySelector('.highscore')
+const awnserBox = document.querySelector('.number')
+const againButton = document.querySelector('.again')
 
 //assign js var
 let scoreCount = 0
-let highscore;
+let highscore = 100
 let isCorrect = false
 
 scoreField.textContent = scoreCount
@@ -33,24 +35,35 @@ function checkIfCorrect () {
     if (Number(correctNumber) === Number(guess)) {
         isCorrect = true
         scoreField.textContent = scoreCount
+        awnserBox.textContent = correctNumber
         updateHighscore(scoreCount)
-        reset()
+    }
+    else if (guess < 1 || guess > 20) {
+        window.alert("number must be between 0 and 20")
     }
     else {
         isCorrect = false
+        scoreField.textContent = scoreCount
     }
     console.log(isCorrect)
 }
 
 function updateHighscore (score) {
-    if (score > highscore) {
+    if (score < highscore) {
         highscore = score
+        highscoreField.textContent = highscore
     }
 }
 
 checkButton.addEventListener('click', checkIfCorrect)
+againButton.addEventListener('click', reset)
+
 
 function reset () {
     correctNumber = getRandomNumber()
     scoreCount = 0
+    scoreField.textContent = scoreCount
+    awnserBox.textContent = '?'
 }
+
+
