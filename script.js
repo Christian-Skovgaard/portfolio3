697443b4e162a5ae966f2f56e4675b5f9a9d4ba8
Christian-Skovgaard <cellochr@gmail.com>
2024-10-07 16:03:22 +0200

    scoresystem working

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 'use strict';
 
 //make correct number ✅
-//make input a let in js
 //compare imputted number and correct number ✅
 //make if-statement that increase if not correct
 //make highscore system work
 //make game reset
 
-
+//assign correct answer
 function getRandomNumber () {
     return Math.floor(Math.random() * 21)
 }
-const correctNumber =getRandomNumber()
+let correctNumber =getRandomNumber()
 
+
+//assign html elements
 const playerInputField =  document.querySelector('input')
 const checkButton = document.querySelector(".check")
+const scoreField = document.querySelector('.score')
+const highscoreField = document.querySelector('.highscore')
 
-
+//assign js var
+let scoreCount = 0
+let highscore;
 let isCorrect = false
 
+scoreField.textContent = scoreCount
+
+//function that runs after check is pressed
 function checkIfCorrect () {
     let guess = playerInputField.value
+    scoreCount++
     if (Number(correctNumber) === Number(guess)) {
         isCorrect = true
+        scoreField.textContent = scoreCount
+        updateHighscore(scoreCount)
+        reset()
     }
     else {
         isCorrect = false
@@ -30,4 +42,15 @@ function checkIfCorrect () {
     console.log(isCorrect)
 }
 
+function updateHighscore (score) {
+    if (score > highscore) {
+        highscore = score
+    }
+}
+
 checkButton.addEventListener('click', checkIfCorrect)
+
+function reset () {
+    correctNumber = getRandomNumber()
+    scoreCount = 0
+}
